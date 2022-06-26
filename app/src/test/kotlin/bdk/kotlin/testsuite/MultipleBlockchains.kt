7f0a22ae85c4dba12169635c92580c8d49144498
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-26 09:29:41 -0400

    Add test for multiple Electrum servers

---
+++ app/src/test/kotlin/bdk/kotlin/testsuite/MultipleBlockchains.kt
@@ -0,0 +1,57 @@
+package bdk.kotlin.testsuite
+
+import org.bitcoindevkit.*
+import org.junit.Test
+import java.nio.file.Files
+
+class MultipleBlockchains {
+    private fun getTestDataDir(): String {
+        return Files.createTempDirectory("bdk-test").toString()
+    }
+
+    private val descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)"
+
+    private val blockchainConfig1 = BlockchainConfig.Electrum(
+        ElectrumConfig(
+            url = "ssl://electrum.blockstream.info:60002",
+            socks5 = null,
+            retry = 5u,
+            timeout = 20u,
+            stopGap = 100u
+        )
+    )
+
+    private val blockchainConfig2 = BlockchainConfig.Electrum(
+        ElectrumConfig(
+            url = "tcp://electrum.blockstream.info:60001",
+            socks5 = null,
+            retry = 5u,
+            timeout = 20u,
+            stopGap = 20u
+        )
+    )
+
+    private val blockchainConfig3 = BlockchainConfig.Electrum(
+        ElectrumConfig(
+            url = "tcp://testnet.aranguren.org:51001",
+            socks5 = null,
+            retry = 5u,
+            timeout = 20u,
+            stopGap = 20u
+        )
+    )
+
+    @Test
+    fun `Create 3 Electrum Blockchains`() {
+        val testDataDir = getTestDataDir() + "/bdk-wallet.sqlite"
+        val databaseConfig = DatabaseConfig.Sqlite(SqliteDbConfiguration(testDataDir))
+        val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
+        val blockchain = Blockchain(blockchainConfig1)
+        wallet.sync(blockchain, LogProgress)
+        val blockchain2 = Blockchain(blockchainConfig2)
+        wallet.sync(blockchain2, LogProgress)
+        val blockchain3 = Blockchain(blockchainConfig3)
+        wallet.sync(blockchain3, LogProgress)
+    }
+
+}
