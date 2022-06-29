80d4ab11583ed97c10012928266275dd225b3445
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-29 13:12:03 -0400

    Add sending to multiple recipients using a list test

--- app/src/test/kotlin/bdk/kotlin/testsuite/MulitpleRecipients.kt
+++ app/src/test/kotlin/bdk/kotlin/testsuite/MulitpleRecipients.kt
@@ -4,6 +4,7 @@ import kotlin.test.assertNotNull
 import org.bitcoindevkit.*
 import kotlin.test.Test
 import java.nio.file.Files
+import kotlin.test.assertTrue
 import kotlin.test.fail
 
 class MultipleRecipients {
@@ -49,4 +50,37 @@ class MultipleRecipients {
     //         fail("Wallet balance is below 3000 sats (current balance: $balance, send more to ${addressInfo.address} (index ${addressInfo.index}")
     //     }
     // }
+
+    @Test
+    fun `Send to multiple recipients with list`() {
+        val descriptor = "wpkh(tprv8hwWMmPE4BVNxGdVt3HhEERZhondQvodUY7Ajyseyhudr4WabJqWKWLr4Wi2r26CDaNCQhhxEftEaNzz7dPGhWuKFU4VULesmhEfZYyBXdE/0/*)"
+        val testDataDir = getTestDataDir() + "/bdk-wallet.sqlite"
+        val databaseConfig = DatabaseConfig.Sqlite(SqliteDbConfiguration(testDataDir))
+        val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
+        val blockchain = Blockchain(blockchainConfig)
+        wallet.sync(blockchain, LogProgress)
+        val balance = wallet.getBalance()
+
+        if (balance > 3000u) {
+            val recipientList = listOf<Recipient>(
+                Recipient("tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt", 1000uL),
+                Recipient("mv4rnyY3Su5gjcDNzbMLKBQkBicCtHUtFB", 1000uL)
+            )
+
+            val psbt = recipientList.fold(TxBuilder()) { builder, recipient ->
+                builder.addRecipient(recipient.address, recipient.amount)
+            }.finish(wallet)
+
+            val signatureWorked: Boolean = wallet.sign(psbt)
+            blockchain.broadcast(psbt)
+            val txid = psbt.txid()
+            println("https://mempool.space/testnet/tx/$txid")
+            assertTrue(signatureWorked)
+        } else {
+            val addressInfo = wallet.getAddress(AddressIndex.LAST_UNUSED)
+            fail("Wallet balance is below 3000 sats (current balance: $balance, send more to ${addressInfo.address} (index ${addressInfo.index}")
+        }
+    }
 }
+
+data class Recipient(val address: String, val amount: ULong)
